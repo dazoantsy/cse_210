8a36d514b7c9ee4119a8cf4c299242ec66351e06
dazoantsy <dazoantsy@gmail.com>
2023-04-19 22:38:22 +0300

    W01 activities

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage (0 - 100) ");
+        string gradePercentage = Console.ReadLine();
+        int gradeInt = int.Parse(gradePercentage);
+        string letter = "";
+
+        if (gradeInt >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (gradeInt >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (gradeInt >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (gradeInt >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("Congratulation, you have passed.");
+        }
+        else
+        {
+            Console.WriteLine("We are sorry that you could not pass, you will surely pass next time.");
+        }
     }
 }
