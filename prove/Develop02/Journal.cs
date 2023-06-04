778e266404ed61a6b6c00127903d7d47d9f2b3f1
dazoantsy <dazoantsy@gmail.com>
2023-06-04 12:00:13 +0300

    fix save issue

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -3,7 +3,7 @@ using System.IO;
 public class Journal
 {
     public List<Entry> _journal = new List<Entry>();
-    public string _userFileName;
+    //public string _userFileName;
     public Journal()
     {
     }
@@ -64,9 +64,13 @@ public class Journal
         if (!File.Exists(_userFileName))
         {
             File.CreateText(_userFileName);
+            Console.Write($"\nYour file has been created and saved as {_userFileName} !\n");
+            SaveFile(_userFileName);
         }
         else
         {
+            Console.Write($"\nA file named {_userFileName} already exits.\n");
+            Console.Write("Your journal entries have been added to it.\n");
             AddFile(_userFileName);
         }
     }
